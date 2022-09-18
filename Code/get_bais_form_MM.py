a45f16840ecfe1515737ded02104b2d30b7545b1
alsowbdxa <78086660+alsowbdxa@users.noreply.github.com>
2022-09-18 15:39:28 +0200

    Update get_bais_form_MM.py

--- Code/get_bais_form_MM.py
+++ Code/get_bais_form_MM.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 26 09:38:15 2021
-
-@author: Daoud
 """
 import os
 import requests
@@ -54,12 +51,12 @@ header = {
     'Sec-Fetch-Dest': 'empty',
     'Referer': 'https://twitter-app.mpi-sws.org/media-bias-monitor/search.php?query=worcest',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cookie': '_ga=GA1.2.1663066557.1637149012; _gid=GA1.2.1002822403.1639127087; demographic=vh7gk2uafudeqhoe8era1jans6'
+    'Cookie': #use youself cookie
 }
 #read the domain file
-file_name = r"E:\Project 2(Bias and reability on wiki and news)/top10w_news_source.txt"
+file_name = #input the domain name to extract the data from Media Bias Monitor, which you can get from the main dataset: Wikipedia Citations
 list_domain = []
-for line in open(file_name,"r"): #设置文件对象并读取每一行文件
+for line in open(file_name,"r"): 
     list_domain.append(line.strip())
 
 # send the parm to the url and get it's url and interest_id,which could be used to 
@@ -96,64 +93,23 @@ def get_detail(interest_id):
         res1 = requests.post(url=details_url,data=payload,headers=header)
         res1 = json.loads(res1.text)
     return res1
-
-#get ip
-def get_ip():
-    url='http://api.89ip.cn/tqdl.html?api=1&num=1000&port=&address=&isp='
-    header={
-                'User-Agent': random.choice(ua)
-                ,'Connection': 'close'
-                ,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-                ,'Accept-Encoding': 'gzip, deflate'
-                ,'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'
-                }    
-    html = requests.get(url,headers=header,timeout=30).content.decode('utf8')           
-    d = BeautifulSoup(html,'html.parser')   
-    ip = str(d).split('<br/>')[2:-1]
-    return ip
-
-ip = get_ip()
-#
-qc = []
-try:
-    for line in open(r"E:\Project 2(Bias and reability on wiki and news)\qc.txt","r",encoding='utf8'): #设置文件对象并读取每一行文件
-        qc.append(line.strip())
-except:
-    pass
-#start to extract, here the list_domain is a unique list of domain name, size is 
-# 1155922 (remove the '')
+##############################################################################################
+#start to extract, here the list_domain is a unique list of domain name
 page_1 = dict()#this dictionary is to store the relationship between domain and
                #related results, we use the interesi_id here to get details of domain
 page_2 = dict()#this dict is use to store the detail information for each results
 short_break = 1
 
-#0-2.5w:console4, finished 16461
-#2.5-5:console 3 # finished
-
-for i in tqdm(list_domain[:25000]):
+for i in tqdm(list_domain):
     if i in page_1:
-        continue
-    if i in qc:
-        continue
-    # if short_break >100:
-    #     time.sleep(random.randint(2,4))
-    #     short_break = 1
-    # else:
-    #     short_break+=1 
-    # if list_domain.index(i)%5000==0:
-    #     ip = get_ip()        
+        continue   
     while 1:       
        try: 
             header['User-Agent'] = random.choice(ua)
             result_1 = search_news(i)
-            # time.sleep(random.randint(1,3))
             if result_1['status'] == 'ok':
                 page_1[i] = result_1
-                qc.append(i)
-                # with open(r"E:\Project 2(Bias and reability on wiki and news)/page1_4.csv",'a',newline='',encoding='utf8') as f:
-                #     writer = csv.writer(f)
-                #     writer.writerow([i,result_1])
-                with open(r"E:\Project 2(Bias and reability on wiki and news)/page1_1.txt",'a',encoding='utf8') as f:
+                with open(r"MM_page1.txt",'a',encoding='utf8') as f:
                     f.write(str([i,result_1]))
                     f.write('\n')
                 if result_1['response'] == '' or result_1['response'] == []:
@@ -163,12 +119,9 @@ for i in tqdm(list_domain[:25000]):
                     for info in tqdm(result_1['response'][:100]):
                         detail = get_detail(info['interest_id'])
                         t[info['title']] = detail
-                        # time.sleep(random.randint(1,3))
                     page_2[i] = t
-                    # with open(r"E:\Project 2(Bias and reability on wiki and news)/page2_4.csv",'a',newline='',encoding='utf8') as f:
-                    #     writer = csv.writer(f)
-                    #     writer.writerow([i,t])
-                    with open(r"E:\Project 2(Bias and reability on wiki and news)/page2_1.txt",'a',encoding='utf8') as f:
+                  
+                    with open(r"MM_page2.txt",'a',encoding='utf8') as f:
                         f.write(str([i,t]))
                         f.write('\n')
                     break
@@ -181,16 +134,9 @@ for i in tqdm(list_domain[:25000]):
            break
        else:
            short_break+=1 
-    # except:
-    #         time.sleep(5)
-    #         proxy = {'http':random.choice(ip)}
-    #         url_ck = requests.get('https://twitter-app.mpi-sws.org/media-bias-monitor/search.php?query='+random.choice(list_domain),headers=header,proxies=proxy)
-    #         ck = requests.utils.dict_from_cookiejar(url_ck.cookies)
-    #         old_ck = '_ga=GA1.2.1663066557.1637149012; _gid=GA1.2.1002822403.1639127087'
-    #         for k,v in ck.items():
-    #             new_ck = old_ck+'='.join([k,v])
-    #         header['Cookie'] = new_ck
+#save the data and they will be used later in analysis file.
+with open("MM_page1_dict.pkl",'wb') as f:
+    pickle.dump(page_1,f)
 
-with open(r"E:\Project 2(Bias and reability on wiki and news)\qc.txt", 'a',encoding='utf-8') as f:
-    for i in qc:
-        f.write(i+'\n')
+with open(r"MM_page2_dict.pkl",'wb') as f:
+    pickle.dump(page_2,f)
